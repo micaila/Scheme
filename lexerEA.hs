4ac2f15446ce08fb2e849f28d63ff234760eabd0
Samantha Mondragon <samantha@Mac-mini-de-Samantha.local>
2020-05-06 13:50:13 -0500

    Archivos iniciales
    Estos son los archivos que hay proporciono la maestra y que hay que modificar

---
+++ lexerEA.hs
@@ -0,0 +1,60 @@
+module LexerEA(Token(..), PalRsv(..),  lexer, lexDigit) where
+
+import Data.Char
+
+{- ***************************************************************
+   Definición del tipo de dato Token para clasificar cada uno de los
+   componentes léxicos, los cuales pueden ser:
+   a) Parentesis que abre y que cierra
+   b) Números
+   c) Operadores (+,*) 
+   d) Palabras reservedas: Suc y Pred }
+   ****************************************************************-}
+
+data Token = ParA | ParC | Lit Int | Oper Char | Rsv PalRsv | Unkwn Char
+    deriving Show
+
+data PalRsv = Suc | Pred
+    deriving Show
+
+{- **************************************************************
+   La función (lexer xs) recibe una cadena que corresponde a una 
+   expresión aritmética en sintaxis concreta, conforme a las reglas
+   de la gramática: 
+     EA ::= T + EA | T
+      T ::= F * T  | F
+      F ::= Num | (EA) | Suc EA | Pred EA 
+   Y devuelve la secuencia de tokens correspondientes.
+   Por ejemplo: 
+   *LexerEA> lexer "3+8*7"
+    [Lit 3,Oper '+',Lit 8,Oper '*',Lit 7]
+   *LexerEA> lexer "(3+8)*suc 7"
+    [ParA,Lit 3,Oper '+',Lit 8,ParC,Oper '*',Rsv Suc,Lit 7]
+   *LexerEA> lexer "(3+8)*suc 7+4"
+    [ParA,Lit 3,Oper '+',Lit 8,ParC,Oper '*',Rsv Suc,Lit 7,Oper '+',Lit 4]
+   *LexerEA> lexer "( 3 + 8 ) * suc ( 7 + 4 )"
+    [ParA,Lit 3,Oper '+',Lit 8,ParC,Oper '*',Rsv Suc,ParA,Lit 7,Oper '+',Lit 4,ParC]
+
+   **************************************************************-}
+lexer :: String -> [Token]
+lexer [] = []
+lexer (' ' : xs) = (lexer xs)
+lexer ('(' : xs) = ParA:(lexer xs)
+lexer (')' : xs) = ParC:(lexer xs)
+lexer ('+' : xs) = (Oper '+'):(lexer xs)
+lexer ('-' : xs) = (Oper '-'):(lexer xs)
+lexer ('*' : xs) = (Oper '*'):(lexer xs)
+lexer ('/' : xs) = (Oper '/'):(lexer xs)
+lexer ('p':'r':'e':'d':xs) = (Rsv Pred):(lexer xs)
+lexer ('s':'u':'c':xs) = (Rsv Suc):(lexer xs)
+lexer (x:xs)
+    | isDigit (x) = lexDigit x xs
+    | otherwise = (Unkwn x):(lexer xs)
+
+lexDigit x xs = let (digitos, resto) = break notDigit (x:xs);
+					  notDigit d = not (isDigit d);
+		 			  valDigit x = (ord x) - (ord '0');
+		              valDigits n [] = n;
+		              valDigits n (c:cs) = valDigits (10 * n + (valDigit c)) cs
+		        in 
+	               (Lit (valDigits 0 digitos)):(lexer resto)
---
+++ perserEA.hs
@@ -0,0 +1,157 @@
+
+module ParserEA where
+
+import LexerEA
+
+{- ***************************************************************
+   Definición del tipo de dato AsaEA, que corresponde a un árbol
+   de sintáxis abstracta de una expresión aritmética, 
+   por la siguiente gramática. 
+     AsaEA ::= Cons Int             (Para representar los números)
+              | Sum AsaEA AsaEA     (Para representar las sumas)  
+              | Prod AsaEA AsaEA    (Para representar las multiplicaciones)  
+              | Scs AsaEA           (Para representar la función sucesor)  
+              | Prd AsaEA           (Para representar la función predecesor)  
+   ****************************************************************-}
+
+data AsaEA = Const Int | Sum AsaEA AsaEA | Prod AsaEA AsaEA | Scs AsaEA | Prd AsaEA
+             deriving Show
+
+{- ***************************************************************
+   La función (parser t) recibe una lista de tokens y construye el 
+   árbol de sintáxis abstracta correspondiente a la expresión,
+   siempre y cuando está corresponda a las reglas de la gramática
+     EA ::= T + EA | T
+      T ::= F * T  | F
+      F ::= Num | (EA) | Suc EA | Pred EA 
+  Para esto, utiliza funciones axiliares que se encargan de 
+  analizar cada categoria.
+
+-- Por ejemplo: 
+--
+-- ParserEA> parser [Lit 3,Oper '+',Lit 8,Oper '*',Lit 7]
+--  Sum (Const 3) (Prod (Const 8) (Const 7))
+--
+-- ParserEA> parser (lexer "3+8*7+suc (2*1)")
+--  Sum (Const 3) 
+--      (Sum (Prod (Const 8) (Const 7)) 
+--           (Scs (Prod (Const 2) (Const 1))))
+--
+-- ParserEA> parser (lexer "3+8*7+suc 2*1")
+--  Sum (Const 3) 
+--      (Sum (Prod (Const 8) (Const 7)) 
+--           (Prod (Scs (Const 2)) (Const 1)))
+--
+
+  ****************************************************************-}
+parser :: [Token] -> AsaEA
+parser t = 
+  case (parserE t) of
+    (expr, []) -> expr
+    _          -> error "Análisis sintáctico fallido."
+
+
+
+-- *************************************************************
+-- (parserE t), recibe una lista de tokens t, y devuelve una
+-- una un AsaEA con la primera expresión que puede formar a 
+-- partir de t y el resto de tokens que faltan por analizar  
+--
+-- Por ejemplo: 
+--
+-- ParserEA> parserE (lexer "4*9)+2")
+--(Prod (Const 4) (Const 9),[ParC,Oper '+',Lit 2])
+-- ParserEA> parserE (lexer "suc 4*9)+2")
+--(Prod (Scs (Const 4)) (Const 9),[ParC,Oper '+',Lit 2])
+-- ParserEA> parserE (lexer "suc 4+9)+2")
+--(Sum (Scs (Const 4)) (Const 9),[ParC,Oper '+',Lit 2])
+--
+-- ************************************************************
+parserE::[Token] -> (AsaEA, [Token])      
+parserE tokens =                        
+  case rst of                          
+     []                  -> (e1', [])    
+     (Oper '+'):rst'    -> let         
+                            (e2', rst'') =  parserE rst'  
+                          in (Sum e1' e2', rst'')  
+     _  -> (e1', rst)
+   where 
+    (e1', rst) = parserT tokens
+
+
+-- *************************************************************
+-- (parserT t), analiza expresiones de la categoria T (términos) 
+-- recibe una lista de tokens t, y devuelve un
+--  AsaEA con la primera expresión con un producto 
+--  y el resto de tokens que faltan por analizar  
+--
+-- Por ejemplo: 
+--
+--ParserEA> parserT (lexer "(4+9)+2")
+--(Sum (Const 4) (Const 9),[Oper '+',Lit 2])
+--ParserEA> parserT (lexer "3*2)+3")
+--(Prod (Const 3) (Const 2),[ParC,Oper '+',Lit 3])
+--ParserEA> parserT (lexer "4+9)+2")
+--(Const 4,[Oper '+',Lit 9,ParC,Oper '+',Lit 2])
+--ParserEA> parserT (lexer "suc 4+9)+2")
+--(Scs (Const 4),[Oper '+',Lit 9,ParC,Oper '+',Lit 2])
+--ParserEA> parserT (lexer "suc (4+9)+2")
+--(Scs (Sum (Const 4) (Const 9)),[Oper '+',Lit 2])
+--
+-- ************************************************************
+
+parserT:: [Token] -> (AsaEA, [Token])    
+parserT tokens =                       
+  case rst of 
+    []                  -> (e1', [])
+    (Oper '*'):rst'     -> let
+                             (e2', rst'') =  parserF rst'
+                           in (Prod e1' e2', rst'')
+    _                   -> (e1',rst)
+
+  where 
+    (e1', rst) = parserF tokens
+
+
+-- *************************************************************
+-- (parserT t), analiza expresiones de la categoria F (factores) 
+-- recibe una lista de tokens t, y devuelve un
+--  AsaEA con la primera expresión de la categoria F 
+--  y el resto de tokens que faltan por analizar  
+--
+-- Por ejemplo: 
+--
+--ParserEA> parserF (lexer "2+3")
+--(Const 2,[Oper '+',Lit 3])
+--ParserEA> parserF (lexer "3*2+3")
+--(Const 3,[Oper '*',Lit 2,Oper '+',Lit 3])
+--ParserEA> parserF (lexer "(3*2)+3")
+--(Prod (Const 3) (Const 2),[Oper '+',Lit 3])
+--ParserEA> parserF (lexer "suc 3*2)+3")
+--(Scs (Const 3),[Oper '*',Lit 2,ParC,Oper '+',Lit 3])
+--
+-- ************************************************************
+parserF:: [Token] -> (AsaEA, [Token])   
+
+parserF ((Lit n):tkns) = (Const n, tkns) -- Literales numéricas
+
+parserF (ParA:tkns)     =                   --- Expresiones parentizadas (E)
+  let
+    (e', restTkns) = parserE tkns
+  in case  restTkns of
+    (ParC: restTkns') -> (e',  restTkns')
+    _                -> error ("falta un paréntesis que cierra " ++ show restTkns)
+
+
+parserF (Rsv Suc:tokens) = (Scs tkns2, rest)   -- Sucesor
+ where
+--  (Op Osuc) : tkns1 = tokens
+  (tkns2,rest) = parserF tokens
+
+
+parserF (Rsv Pred:tokens) = (Prd tkns2, rest)  -- Predecesor
+ where
+  (tkns2,rest) = parserF tokens
+
+
+parserF tokens = error ("Error gramatical iniciando en : " ++ show tokens)
